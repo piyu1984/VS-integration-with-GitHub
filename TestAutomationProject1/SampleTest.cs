071fd6b67239012df5f2a432b56b1207090d395a
Priyanka Bhosale <Priyanka.Bhosale@bd.com>
2019-11-10 21:51:23 -0800

    Test project

---
+++ TestAutomationProject1/SampleTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAutomationProject1
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
